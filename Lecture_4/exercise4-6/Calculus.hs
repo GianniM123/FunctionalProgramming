8e0a12e21a3e94c345a9393fc7a4f98f4d8f772d
Martijn Vogelaar <MJ.vogelaar@student.han.nl>
2019-09-27 09:17:58 +0200

    Added more simplifications

--- Lecture_4/exercise4-6/Calculus.hs
+++ Lecture_4/exercise4-6/Calculus.hs
@@ -37,6 +37,7 @@ apply i v = v
 derive   :: Function -> Function
 derive (f :+: g) = derive f :+: derive g 
 derive (f :*: g) = derive f :*: g :+: derive g :*: f
+derive (l :.: r) = (derive l :.: r) :*: derive r
 derive (Prim p)
  | p == Sin = Prim Cos
  | p == Cos = Const (-1) :*: Prim Sin
@@ -45,13 +46,23 @@ derive (Const c) = Const (0)
 derive i = Const (1)
 
 
+-- simplify (Id :*: Id :*: Id :*: Id :*: Const(2) :*: Const(54) :*: Const(4)  :+: Id :*: Id :*: Id :*: Id :*:  Const(53) :+: Id :*: Const(3) :*: Const(0))
 simplify :: Function -> Function
 simplify (Const 0 :*: _) = Const 0
 simplify (_ :*: Const 0) = Const 0
+simplify (Const 1 :*: f) = simplify(f)
+simplify (f :*: Const 1) = simplify(f)
+simplify (Const x :*: Id :+: Const y :*: Id ) = Const(x + y) :*: Id
+simplify (Const x :*: Id :*: f :+: Const y :*: Id :*: g) = if(f==g) then (Const(x + y) :*: Id :*: f) else (Const x :*: Id :*: f :+: Const y :*: Id :*: g)
+simplify (f :*: Const x :*: Const y) = simplify(f :*: Const (x*y))
+simplify (f :*: Const x :*: Const y :+: g) = simplify(f :*: Const (x*y) :+: g)
+simplify (f :*: Const x :+: g :*: Const y) = if(f==g) then (f :*: Const(x+y)) else (f :*: Const x :+: g :*: Const y)
 simplify (Const x :*: Const y) = Const (x*y)
-simplify (x :*: y) = simplify (simplify x :*: simplify y)
+simplify (x :*: y) =  (simplify x :*: simplify y)
 simplify (Const x :+: Const y) =  Const (x+y)
 simplify (Const (0) :+: f) = simplify f
 simplify (f :+: Const (0)) = simplify f
-simplify ((f) :+: (g)) = if (f==g) then (Const (2) :*: (g))  else (simplify(f) :+: simplify(g)) 
+simplify (Const x :+: f) = Const(x) :+: simplify f
+simplify (f :+: Const x) = simplify f :+: Const(x)
+simplify ((f) :+: (g)) = if (f==g) then (Const (2) :*: (g))  else ((simplify(f) :+: simplify(g)))
 simplify (f) = f
