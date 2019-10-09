cb8bd7008ac8cd095c7aacf37601e296f45940bc
GianniM123 <G.Monteban@student.han.nl>
2019-10-09 20:19:52 +0200

    sorting not working for ex4.2??

--- Lecture_6/exercise6-2/MapReduce.hs
+++ Lecture_6/exercise6-2/MapReduce.hs
@@ -2,16 +2,55 @@ module MapReduce
 where
 import Hardware
 import Data.Monoid
+import Data.List
 
 reduce  ::  (Monoid m) => [m] -> m
 reduce  =  foldr (<>) mempty 
 
+{-
+x || y              OR
+!x || y
+x || !y
+!x || !y
+x && y              AND
+!x && y
+x && !y
+!x && !y
+x
+y
+!x
+!y
+True
+False
+x || y && not x && y
+!(x || y && not x && y)
+-}
 
-instance Monoid Bool where
-    mempty = False
-    x `mappend` y = x Prelude.|| y
 
-newtype OrdList elem = Ord [elem]
+newtype Bool1 = B1 {bool :: Bool }
+    deriving (Show)
+
+instance Monoid Bool1 where
+    mempty = B1 False
+    x `mappend` y = B1 (bool x Prelude.|| bool y)
+
+
+
+newtype Additive = S1 { fromSum :: Int}
+    deriving (Show)
+
+instance Monoid Additive where
+    mempty = S1 0
+    x `mappend` y = S1 (fromSum x + fromSum y)
+
+
+newtype OrdList elem = Ord {list::[elem]}
+    deriving (Show)
+
+instance (Ord elem) => Monoid (OrdList elem) where
+    mempty = Ord []
+    x `mappend` y = Ord ( sort (list x ++ list y))
+
 
 -- instance (Ord elem) => Monoid (OrdList elem) where
 
