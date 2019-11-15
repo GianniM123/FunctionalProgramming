01380387e127d1cf937060e5f25c2b5f17094def
GianniM123 <G.Monteban@student.han.nl>
2019-11-15 10:00:28 +0100

    added 2 version mastermind

--- Lecture_9/Exercise10.2/Mastermind.hs
+++ Lecture_9/Exercise10.2/Mastermind.hs
@@ -2,54 +2,79 @@ module Main
 where
 import System.Random
 import Control.Monad
+import System.IO
+import Data.List
 import Data.List.Split
 
-data Colors = White | Silver | Green | Red | Orange | Pink | Yellow | Blue deriving (Eq,Show)
+data Colors = White | Silver | Green | Red | Orange | Pink | Yellow | Blue deriving (Eq,Ord,Show)
 listColors = [White,Silver,Green,Red,Orange,Pink,Yellow,Blue]
 
 data Symbol = A | B | D | F | G deriving (Eq,Show)
 symbolList = [A,B,D,F,G]
 
-codeLength = 4
-maxTries = 12
 
 main :: IO ()
 main = do
+    putStrLn "Welcome to Mastermind, how many guesses do you want?"
+    maxTries <- readLn
+    putStrLn "Okay and how long should the code length be?"
+    codeLength <- readLn
     putStr "I think of a "
     putStr . show $ codeLength
     putStr " colors, make a guess you have a maximum of "
     putStr . show $ maxTries
     putStrLn " tries."
     colors <- randomColors codeLength listColors
-    -- print colors
-    gameLoop colors 0
+    y <- gameLoop colors 0 maxTries codeLength
+    putStrLn y
+    putStr "The solution was: "
+    print colors
 
-gameLoop :: [Colors] -> Int -> IO()
-gameLoop colors tries = do
+gameLoop :: [Colors] -> Int -> Int -> Int -> IO String
+gameLoop colors tries maxTries codeLength = do
     guess <- getLine
     let guessedColors = map parseInput (splitOn " " guess)
-    let correct = checkCorrectInput colors guessedColors
-    let rightColors = ((checkCorrectColor colors guessedColors) - correct)
+    let correct = checkCorrectInput colors guessedColors 0
+    let incorrectValues = removeFromList colors correct 0
+    let incorrectGuesses = removeFromList guessedColors correct 0
+    let rightColors = (checkCorrectColor incorrectValues incorrectGuesses)
     putStr "Amount of places correct: "
-    print correct
+    print (length correct)
 
     putStr "Amount of colors correct: "
     print rightColors
-    if(correct == codeLength || tries >= maxTries-1) then
-        return ()
+    putStrLn ""
+    if (length correct == codeLength) then
+        return "You have won!"
     else
-       gameLoop colors (tries+1)
+        if (tries >= maxTries-1) then
+            return "you have lost!"
+        else
+            gameLoop colors (tries+1) maxTries codeLength
 
-checkCorrectInput :: [Colors] -> [Colors] -> Int
-checkCorrectInput (x:[]) (y:[]) = if x == y then 1 else 0
-checkCorrectInput (x:xs) (y:[]) = error("Unequal length")
-checkCorrectInput (x:[]) (y:ys) = error("Unequal length")
-checkCorrectInput (x:xs) (y:ys) = if x == y then 1+(checkCorrectInput xs ys) else 0+(checkCorrectInput xs ys)
+removeFromList :: [Colors] -> [Int] -> Int -> [Colors]
+removeFromList [] [] _ = []
+removeFromList x [] _ = x
+removeFromList (x:xs) (y:ys) i = if (y == i) then removeFromList xs ys (i+1) else [x] ++ removeFromList xs (y:ys) (i+1)
+
+checkCorrectInput :: [Colors] -> [Colors] -> Int -> [Int]
+checkCorrectInput (x:[]) (y:[]) i = if x == y then [i] else []
+checkCorrectInput (x:xs) (y:[]) i = error("Unequal length")
+checkCorrectInput (x:[]) (y:ys) i = error("Unequal length")
+checkCorrectInput (x:xs) (y:ys) i = if x == y then [i] ++ (checkCorrectInput xs ys (i+1)) else (checkCorrectInput xs ys (i+1))
+
+checkInList :: [Colors] -> [[Colors]] -> Int
+checkInList [] _ = 0
+checkInList _ [] = 0
+checkInList x (y:ys) 
+ | (head x == head y) = min (length x) (length y)
+ | otherwise = checkInList x ys
 
 checkCorrectColor :: [Colors] -> [Colors] -> Int
-checkCorrectColor (x:[]) y = length (filter (==x) y)
-checkCorrectColor (x:xs) y = length (filter (==x) y) + (checkCorrectColor xs y)
-    
+checkCorrectColor x y = sum (map (\z -> checkInList z nY ) nX)
+    where nX = group . sort $ x 
+          nY = group . sort $ y
+
 parseInput :: String -> Colors
 parseInput x
  | x == "White" = White
