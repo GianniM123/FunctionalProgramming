0d296b4fe5e23b85ccdc3bb4584f92af592a122f
GianniM123 <G.Monteban@student.han.nl>
2019-11-22 10:57:06 +0100

    added 11.2

--- Lecture_10/exercise11-2/Evaluator.hs
+++ Lecture_10/exercise11-2/Evaluator.hs
@@ -1,5 +1,11 @@
+{-
+Gianni Monteban & Martijn Vogelar
+1047546 & 1047391
+-}
+
 module Evaluator
 where
+import Data.Maybe
 
 infixl 6 :+:
 infixl 7 :*:
@@ -27,7 +33,7 @@ evalN (Lit i)      =  pure i
 evalN (e1 :+: e2)  =  pure (+)  <*> evalN e1 <*> evalN e2
 evalN (e1 :*: e2)  =  pure (*)  <*> evalN e1 <*> evalN e2
 evalN (Div e1 e2)  =  pure div  <*> evalN e1 <*> evalN e2
-evalN (e1 :?: e2)  =  evalA e1 ++ evalA e2
+evalN (e1 :?: e2)  =  evalN e1 ++ evalN e2
 
 -- 
 -- evalN toss
@@ -42,9 +48,11 @@ instance Functor Environ where
 
 instance Applicative Environ where
 -- pure a -> Environ a
-   pure x = error "pure (Applicative Environ): not yet implemented"
+   pure x = EN $ \env -> x 
+    
 -- (<*>) :: Environ (a -> b) -> Environ a -> Environ b 
-   EN f <*> EN x  = error "<*> (Applicative Environ): not yet implemented"
+   EN f <*> EN x  = EN $ \ev -> func ev
+    where func = \env -> f env (x env)
 
 instance Monad Environ where
 -- return a -> Environ a
@@ -53,7 +61,12 @@ instance Monad Environ where
    EN m >>= f = error "<*> (Monad Environ): not yet implemented"
 
 evalR :: Expr -> Environ Integer
-evalR = error "evalR: not yet implemented"
+evalR (Lit i)      =  pure i
+evalR (e1 :+: e2)  =  pure (+)  <*> evalR e1 <*> evalR e2
+evalR (e1 :*: e2)  =  pure (*)  <*> evalR e1 <*> evalR e2
+evalR (Div e1 e2)  =  pure div  <*> evalR e1 <*> evalR e2
+evalR (Var v) = EN (\env -> fromMaybe 0 $ lookup v env)
+-- evalR = error "evalR: not yet implemented"
 -- evalR (Var "a" :+: Lit 1) [("a", 4711), ("b", 0815)]
 -- evalR (Var "a" :*: Var "b") [("a", 4711), ("b", 0815)]
 -- evalR (Var "a" :*: Var "c") [("a", 4711), ("b", 0815)]
@@ -62,14 +75,20 @@ evalR = error "evalR: not yet implemented"
 newtype EnvND a = EnN { fromEnN :: [(String, Integer)] ->  [a] }
 instance Functor EnvND where
 -- fmap :: (a -> b) -> EnvND a -> EnvND b
-   fmap f (EnN x) = error "fmap (Functor EnvND): not yet implemented"
+   fmap f (EnN x) = EnN $ \en -> map (\y -> f y) (x en) 
 
 instance Applicative EnvND where
 --pure a -> EnvND a
-  pure x = error "pure (Applicative EnvND): not yet implemented"
+  pure x = EnN $ \env -> [x] 
 --(<*>) :: EnvND (a -> b) -> EnvND a -> EnvND b 
-  EnN f <*> EnN xs  = error "<*> (Applicative EnvND): not yet implemented"
+  EnN f <*> EnN xs  = EnN $ \ev -> func ev
+    where func = \env -> [y z | y <- (f env), z <- (xs env)]
 
 evalNR :: Expr -> EnvND Integer
-evalNR = error "evalNR: not yet implemented"
+evalNR (Lit i)      =  pure i
+evalNR (e1 :+: e2)  =  pure (+)  <*> evalNR e1 <*> evalNR e2
+evalNR (e1 :*: e2)  =  pure (*)  <*> evalNR e1 <*> evalNR e2
+evalNR (Div e1 e2)  =  pure div  <*> evalNR e1 <*> evalNR e2
+evalNR (e1 :?: e2)  =  EnN $ \e -> fromEnN (evalNR e1) e ++ fromEnN (evalNR e2) e
+evalNR (Var v) = EnN $ \list -> [fromMaybe 0 $ lookup v list]
 
