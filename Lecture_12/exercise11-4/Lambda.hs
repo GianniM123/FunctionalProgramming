9d44180849ec39421639eeed4ea392599518cfcd
GianniM123 <G.Monteban@student.han.nl>
2019-12-06 09:38:53 +0100

    lambda function

--- Lecture_12/exercise11-4/Lambda.hs
+++ Lecture_12/exercise11-4/Lambda.hs
@@ -5,6 +5,7 @@ Gianni Monteban & Martijn Vogelaar
 
 import Parser
 import Control.Applicative
+import Data.Char
 
 infixl 9 :@
 
@@ -22,9 +23,27 @@ L -> SS
 
 Var var = var
 Fun var (Lambda var) = \\var -> var
-Lambda var :@ Lambda var = varvar
+Lambda var :@ Lambda var = var var
 -}
+
+readString :: Parser String
+readString = do
+  x <- letter
+  xs <- many letter
+  return (x:xs)
+
+parseArrow :: Parser ()
+parseArrow = do
+  x0 <- space
+  x1 <- char '-'
+  x2 <- char '>'
+  x3 <- space
+  return ()
 
 parseToLambda :: String -> Lambda String
 parseToLambda (x:xs)
- | x == '\\' = undefined
+ | isSpace x = parseToLambda xs
+ | isAlpha x = let var = ((parse readString (x:xs)) !! 0) in if length (snd var) > 0 then Var (fst var) :@ parseToLambda (snd var) else Var (fst var)
+ | x == '\\' = let var = (parse readString xs) !! 0 in let var2 = snd( parse parseArrow(snd var) !! 0) in Fun (fst var) (parseToLambda var2) 
+ | otherwise = error ("no clue what is happening")                                           
+
