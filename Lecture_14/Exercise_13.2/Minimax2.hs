5981891f9422a87207da5222d774aa9fb03bf5ff
Martijn Vogelaar <MJ.vogelaar@student.han.nl>
2019-12-20 08:22:50 +0100

    Almost done with 13.2

---
+++ Lecture_14/Exercise_13.2/Minimax2.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Minimax2
+where
+import Squiggol
+
+data Tree elem  =  Node elem [Tree elem]
+
+data TREE elem tr = NODE elem [tr] 
+
+instance Functor (TREE elem) where
+    fmap f (NODE elem rs) = NODE elem (map f rs)
+
+instance Base (TREE elem) where
+    type Rec (TREE elem) = Tree elem
+    inn (NODE elem rs) = Node elem rs
+    out (Node elem rs) = NODE elem rs
+
+size, depth :: Tree elem -> Integer
+size = fold $ \(NODE e t) -> sum t + 1
+depth = fold $ \(NODE e t) -> 1 + maximum t
+
+gametree :: (position -> [position]) -> (position -> Tree position)
+gametree f = unfold (\position -> NODE position (f position))
+
+winning  :: Tree position -> Bool
+winning = error "winning not yet defined"
