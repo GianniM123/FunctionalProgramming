eb93f11f8bc3c3edde3b29fc64e7fb98ec7f2785
GianniM123 <G.Monteban@student.han.nl>
2019-12-20 09:02:48 +0100

    false

--- Lecture_14/Exercise_13.2/Minimax2.hs
+++ Lecture_14/Exercise_13.2/Minimax2.hs
@@ -9,6 +9,9 @@ data Tree elem  =  Node elem [Tree elem]
 
 data TREE elem tr = NODE elem [tr] 
 
+
+t = Node "S" [Node "a" [Node "c" []], Node "b" []]
+
 instance Functor (TREE elem) where
     fmap f (NODE elem rs) = NODE elem (map f rs)
 
@@ -19,10 +22,12 @@ instance Base (TREE elem) where
 
 size, depth :: Tree elem -> Integer
 size = fold $ \(NODE e t) -> sum t + 1
-depth = fold $ \(NODE e t) -> 1 + maximum t
+depth = fold f
+    where f (NODE e []) = 1
+          f (NODE e t) = 1 + maximum t
 
 gametree :: (position -> [position]) -> (position -> Tree position)
 gametree f = unfold (\position -> NODE position (f position))
 
 winning  :: Tree position -> Bool
-winning = error "winning not yet defined"
+winning _ = True
